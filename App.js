6313c5d4faea553e7183f9105959e9d42665ef76
BaseBenjamin13 <benmorgiewicz@gmail.com>
2022-09-01 17:21:23 -0400

    init, testing react native out

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+const image = { uri: "https://i.imgur.com/uVkX2tR_d.webp?maxwidth=128&shape=square" };
 
 export default function App() {
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+        <View>
+            <Text>Another text</Text>
+        </View>
+        <Text>Hello World</Text>
+            
+        <Button title="Tap Me!" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+
+    container: {
+        flex: 1,
+        backgroundColor: 'blue',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+
 });
